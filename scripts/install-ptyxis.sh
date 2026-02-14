d4c325e0165709475f5b257a44fd7b6ff9d20ab3
Sorel Ferris <31798052+sorelferris@users.noreply.github.com>
2026-02-14 09:30:23 +0800

    Simplify installation of flatpak and plugin
    Combine flatpak and gnome-software-plugin-flatpak installation into a single command with the -y flag.

--- scripts/install-ptyxis.sh
+++ scripts/install-ptyxis.sh
@@ -2,8 +2,8 @@
 
 # install flakpak & GNOME Software Flatpak plugin
 sudo apt update
-sudo apt install flatpak
-sudo apt install gnome-software-plugin-flatpak
+sudo apt install flatpak gnome-software-plugin-flatpak -y
+
 # add the Flathub repo
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
